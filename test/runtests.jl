f3848e729d42b1285c8bd07c220b92249a27524d
Dilum Aluthge <dilum@aluthge.com>
2020-01-08 04:42:39 -0500

    Progress commit

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,6 +14,8 @@ using Test
     @testset "public_interface.jl" begin
         MicroCoverage.with_temp_dir() do tmp_depot
             MicroCoverage.with_temp_dir() do tmp_src_directory
+                nonexistent_filename = joinpath(tmp_src_directory,
+                                                "nonexistent.jl")
                 foo_jl_filename = joinpath(tmp_src_directory,
                                            "foo.jl")
                 test_foo_jl_filename = joinpath(tmp_src_directory,
@@ -34,7 +36,9 @@ using Test
                     println(io, "")
                     println(io, "@test foo(1) == \"hello\"")
                 end
-                MicroCoverage.start(foo_jl_filename)
+                @test_throws ArgumentError MicroCoverage.start(nonexistent_filename)
+                @test_throws ArgumentError MicroCoverage.clean(nonexistent_filename)
+                MicroCoverage.start(strip(foo_jl_filename))
                 include(foo_jl_filename)
                 MicroCoverage.preview_coverage(; dump_coverage_io = devnull)
                 include(test_foo_jl_filename)
@@ -47,7 +51,9 @@ using Test
                 @test isfile(foo_jl_microcov_filename)
                 @test ispath(foo_jl_microcov_filename)
                 foo_jl_microcov_filecontents = read(foo_jl_microcov_filename, String)
+                touch(foo_jl_microcov_filename)
                 MicroCoverage.clean(foo_jl_filename)
+                touch(foo_jl_microcov_filename)
                 MicroCoverage.clean(tmp_src_directory)
                 @test foo_jl_microcov_filecontents == string("[1,1,1]                 function foo(x)\n",
                                                              "[1,0,1,0,1]                 if x == 1 || x == 100\n",
