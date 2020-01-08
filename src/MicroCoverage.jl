51c0dba7e4edc90c7c0a9fda534c5c5e6cf6c9b7
Dilum Aluthge <dilum@aluthge.com>
2020-01-08 02:07:33 -0500

    Progress commit

--- src/MicroCoverage.jl
+++ src/MicroCoverage.jl
@@ -10,7 +10,6 @@ include("coverage_write.jl")
 include("instrument.jl")
 include("read_write_julia_files.jl")
 include("restore.jl")
-include("setup.jl")
 include("tracker.jl")
 
 end # end module MicroCoverage
